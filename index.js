132497d676378fa50cfa44a8c9da5e1f6922f339
justice jennings <justice.l.jennings@gmail.com>
2023-11-08 15:18:28 -0500

    added variables for API link and tested successfully.

---
+++ index.js
@@ -0,0 +1,9 @@
+// define variables for API Links & test after each
+const mainApiUrl =  `https://fsa-crud-2aa9294fe819.herokuapp.com/api/2310-fsa-et-web-ft-sf`;
+const apiArtistsURL =  `https://fsa-crud-2aa9294fe819.herokuapp.com/api/2310-fsa-et-web-ft-sf/artists`;
+
+console.log(mainApiUrl)
+
+
+
+
